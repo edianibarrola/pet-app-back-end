bcdef77a5c20e7ae948c359a49b8d962a029b5a2
James-Do9 <dojames3@yahoo.com>
2021-01-25 19:12:05 +0000

    Methods work

--- src/main.py
+++ src/main.py
@@ -25,20 +25,83 @@ setup_admin(app)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
-# generate sitemap with all your endpoints
-@app.route('/')
-def sitemap():
-    return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route('/signup', methods=['POST']) #Adds a user to the list when the user signs up
+def sign_up():
+    body = request.get_json() 
+    info = User(username=body['username'], email=body['email'], password=body['password'], is_active=False) 
+    db.session.add(info) 
+    db.session.commit() 
+    updated_user = User.query.filter_by(email=body['email']) 
+    updated_user = list(map(lambda x: x.serialize(), updated_user))
+    return jsonify(updated_user), 200 
 
+@app.route('/user/<int:id>', methods=['DELETE']) #Deletes a user at the specific ID
+def delete_user(id):
+    body = request.get_json()
+    to_be_deleted = User.query.get(id)
+    db.session.delete(to_be_deleted)
+    db.session.commit()
     response_body = {
-        "msg": "Hello, this is your GET /user response "
+        "msg": "The user has been deleted."
     }
-
     return jsonify(response_body), 200
 
+@app.route('/users', methods=['GET']) #Returns all of the users in a list
+def get_all_users():
+    all_users = User.query.all()
+    all_users = list(map(lambda x: x.serialize(), all_users))
+    response_body = {
+        "msg": "Here are all of the users."
+    }
+    return jsonify(all_users), 200
+
+@app.route('/user/<int:id>', methods=['PUT']) #Updates the user's information at their ID
+def update_user(id):
+    body = request.get_json()
+    to_be_updated = User.query.get(id)
+    if to_be_updated is None:
+        raise APIException('User does not exist', status_code=404)
+    if 'email' in body:
+        to_be_updated.username = body['username']
+        to_be_updated.email = body['email']
+        to_be_updated.password = body['password']
+    db.session.commit()
+    to_be_updated = User.query.get(id)
+    to_be_updated = to_be_updated.serialize()
+    response_body = {
+        "msg": "The user has been updated.",
+        "update": to_be_updated
+    }
+    return jsonify(response_body), 200
+
+@app.route('/login', methods=['PUT']) #Sets the is_active to true when the user logs in
+def login_user():
+    body = request.get_json()
+    to_be_updated = User.query.filter_by(email=body['email']).first()
+    if to_be_updated is None:
+        raise APIException('User does not exist', status_code=404)
+    if 'email' in body:
+        to_be_updated.is_active = True
+    db.session.commit()
+    response_body = {
+        "msg": "The user has logged in.",
+    }
+    return jsonify(response_body), 200
+
+@app.route('/logout', methods=['PUT']) #Sets the is_active to false when the user logs out
+def logout_user():
+    body = request.get_json()
+    to_be_updated = User.query.filter_by(email=body['email']).first()
+    if to_be_updated is None:
+        raise APIException('User does not exist', status_code=404)
+    if 'email' in body:
+        to_be_updated.is_active = False
+    db.session.commit()
+    response_body = {
+        "msg": "The user has logged out.",
+    }
+    return jsonify(response_body), 200
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
--- src/models.py
+++ src/models.py
@@ -7,13 +7,16 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    username = db.Column(db.String(100), unique=False, nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.email
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            "username": self.username,
+            "is_active": self.is_active
             # do not serialize the password, its a security breach
         }
